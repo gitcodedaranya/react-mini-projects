273f35b107d7a8f36b85c2a207d940295713dfc0
gitcodearanya <aranya.webdeveloer@gmail.com>
2026-02-25 21:09:52 +0530

    App jsx  updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Home from './pages/Home'
 import Counter from './pages/Counter'
 import TodoApp from './pages/TodoApp'
 import FormValidationApp from './pages/FormValidation'
+import CounterApp from './pages/CounterApp'
+import ApiExample from './pages/ApiExample'
+import RealFormvalidation from './pages/RealFormvalidation'
 
 function App (){
   return (
@@ -14,6 +17,9 @@ function App (){
         <Route path='/counter' element={< Counter/>} />
         <Route path='/todoapp' element={< TodoApp />} />
         <Route path='/formvalidationapp' element={< FormValidationApp />} />
+        <Route path='counterapp' element={< CounterApp/>} />
+        <Route path="/apicallapp" element={< ApiExample />} />
+        <Route path="/realformvalidation" element={< RealFormvalidation />} />
       </Routes>
     </BrowserRouter>
   );
